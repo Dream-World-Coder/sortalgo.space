a8900a6aa820a7a760a4ab381e7f701e41588427
Dream-World-Coder <dreamy.star.368@gmail.com>
2025-11-26 01:28:45 +0530

    feat: added contributor's page

--- middleware.ts
+++ middleware.ts
@@ -26,24 +26,3 @@ export function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
-// import type { NextRequest } from "next/server";
-// import { NextResponse } from "next/server";
-
-// export function middleware(req: NextRequest) {
-//   const host = req.headers.get("host") ?? "";
-//   const url = req.nextUrl;
-
-//   const parts = host.split(".");
-//   const subdomain = parts.length > 2 ? parts[0] : null;
-
-//   /*
-//   if (subdomain === "animations") {
-//     url.pathname = `/animations${url.pathname}`;
-//     return NextResponse.rewrite(url);
-//   }
-//   // return NextResponse.next();
-//   */
-
-//   url.pathname = `/${subdomain}${url.pathname}`;
-//   return NextResponse.rewrite(url);
-// }
---
+++ src/app/contributors/page.tsx
@@ -0,0 +1,3 @@
+export default async function ContributorsPage() {
+  return <>I am Author</>;
+}
