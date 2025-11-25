bca98b566cf817376280dbfe9c2afe0a48b729bc
Dream-World-Coder <dreamy.star.368@gmail.com>
2025-11-25 21:49:22 +0530

    feat: added subdomain middleware

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const host = req.headers.get("host") ?? "";
+  const url = req.nextUrl;
+
+  const parts = host.split(".");
+  const subdomain = parts.length > 2 ? parts[0] : null;
+
+  if (subdomain === "animations") {
+    url.pathname = `/animations${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
+}
